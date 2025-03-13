670910ee2ea759c4fb6c4ac3c003e1f383ce05c1
Mikotaj Inholland <730282@student.inholland.nl>
2025-03-13 14:32:40 +0100

    added views

---
+++ Someren/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Someren.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Students()
+        {
+            return View();
+        }
+
+        public IActionResult Lecturers()
+        {
+            return View();
+        }   
+    }
+}
